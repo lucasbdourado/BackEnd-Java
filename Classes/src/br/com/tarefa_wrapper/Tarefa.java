1777a0f1ad2f5b33ba49550599327eb07c965b30
Lucas Dourado <douradoblucas@gmail.com>
2023-01-02 13:29:52 -0300

    Add Tarefa Wrapper

---
+++ Classes/src/br/com/tarefa_wrapper/Tarefa.java
@@ -0,0 +1,15 @@
+package br.com.tarefa_wrapper;
+
+public class Tarefa {
+
+    public static void main(String[] args) {
+
+        int num = 1111111111;
+
+        System.out.println("**** "+ num + " ****");
+
+        short num2 = (short) num;
+
+        System.out.println("**** "+ num2 + " ****");
+    }
+}
