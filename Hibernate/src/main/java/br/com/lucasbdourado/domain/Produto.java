84f9d712847179702f656696a2023c62a80af2f7
Lucas Dourado <douradoblucas@gmail.com>
2023-01-12 12:54:22 -0300

    Add Projeto Hibernate

---
+++ Hibernate/src/main/java/br/com/lucasbdourado/domain/Produto.java
@@ -0,0 +1,40 @@
+package br.com.lucasbdourado.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
+    @SequenceGenerator(name = "produto_seq", sequenceName = "sq_produto", initialValue = 1, allocationSize = 1)
+    private Long id;
+    @Column(name = "nome", length = 30, nullable = false)
+    private String nome;
+    @Column(name = "description", length = 255, nullable = true)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
